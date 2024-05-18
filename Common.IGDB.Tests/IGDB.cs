1c5e9fb3bddea685d85740730d06f618611dcf7e
Chris Boot <chris@bootcom.co.uk>
2024-05-18 21:57:03 +0100

    Various unit tests added, More need to be done

---
+++ Common.IGDB.Tests/IGDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Tests
+{
+    public class IGDBTests
+    {
+
+        internal IGDB.IGDB _igdb;
+
+        [SetUp]
+        public void Setup()
+        {
+            _igdb = new IGDB.IGDB("clientId", "clientSecret");
+        }
+
+    }
+}
